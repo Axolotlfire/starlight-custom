04c6f034a346a338ea9728c73c6bb905a7995526
Chris Swithinbank <swithinbank@gmail.com>
2023-05-08 20:55:19 +0200

    Rename to “Starlight” (#29)

---
+++ schema.ts
@@ -0,0 +1,23 @@
+import { z } from 'astro:content';
+
+export function docsSchema() {
+  return z.object({
+    /** The title of the current page. Required. */
+    title: z.string(),
+
+    /**
+     * A short description of the current page’s content. Optional, but recommended.
+     * A good description is 150–160 characters long and outlines the key content
+     * of the page in a clear and engaging way.
+     */
+    description: z.string().optional(),
+
+    /**
+     * Custom URL where a reader can edit this page.
+     * Overrides the `editLink.baseUrl` global config if set.
+     *
+     * Can also be set to `false` to disable showing an edit link on this page.
+     */
+    editUrl: z.union([z.string().url(), z.boolean()]).optional().default(true),
+  });
+}
